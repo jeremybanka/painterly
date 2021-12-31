d6560c9bc085d7e527818ac2147d467142c15063
Jeremy Banka <hello@jeremybanka.com>
2021-12-31 00:22:31 -0800

    🎨 format

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client"
 
+const IN_PRODUCTION = process.env.NODE_ENV === `production`
+
 let db: PrismaClient
 
 declare global {
   /* eslint-disable-next-line no-var*/
   var __db: PrismaClient | undefined
 }
-
-const IN_PRODUCTION = process.env.NODE_ENV === `production`
 
 if (IN_PRODUCTION) {
   db = new PrismaClient()
